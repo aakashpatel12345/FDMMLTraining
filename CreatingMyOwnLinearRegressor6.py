a77dc511fc54b2269ede9008ab1d6a8edf8ef250
aakashpatel12345 <53399027+aakashpatel12345@users.noreply.github.com>
2020-02-03 17:30:56 +0000

    Add files via upload

--- CreatingMyOwnLinearRegressor6.py
+++ CreatingMyOwnLinearRegressor6.py
@@ -7,20 +7,39 @@ Created on Tue Jan 14 14:58:33 2020
 
 import pandas as pd
 import numpy as np
+#import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 
 
-data = pd.read_csv("./my_linear_regressor.csv")
-
-x=np.array(data[["x1" , "x2", "x3"]])
-y=np.array(data[["y"]])
+data = pd.read_csv("./data/medical_costs.csv")
 
 
 
+#split catergorical data
+#first sex - split into 1
+dataEncoded = pd.get_dummies(data,columns = ['sex'], drop_first=True)
 
-def gradient_descent(x,y,learning_rate=0.011,num_iterations = 100):
-    NumOfDependentVariables = x.shape[1]
-    
+#then smoker - split into 1
+dataEncoded = pd.get_dummies(dataEncoded,columns = ['smoker'], drop_first=True)
+
+#then region - split into 3
+dataEncoded = pd.get_dummies(dataEncoded,columns = ['region'], drop_first=True)
+
+#standardisation
+scalar = MinMaxScaler()
+dataScaled = dataEncoded.copy()
+dataScaled[['age', 'bmi', 'children', 'charges']] = \
+scalar.fit_transform(dataEncoded[['age', 'bmi', 'children', 'charges']])
+
+#create independent and dependent variables
+x = np.array(dataScaled[["age","bmi","smoker_yes","children",'region_northwest', 'region_southeast', 'region_southwest']])
+y = np.array(dataScaled["charges"])
+
+NumOfDependentVariables = x.shape[1]
+
+
+def gradient_descent(x,y,learning_rate=0.001,num_iterations = 100):
     #initialise y-intercept and gradient
     m = {}
     y_pred = y*0 
@@ -58,21 +77,19 @@ def gradient_descent(x,y,learning_rate=0.011,num_iterations = 100):
        
        b_grad = -(1/n) * sum(y-y_pred)
        b_curr = b_curr - (learning_rate*b_grad)
-       
-       
-       print ("The gradient of term 1 in this iteration is {}".format(m['m_curr_0'][0]))
-       print ("The gradient of term 2 in this iteration is {}".format(m['m_curr_1'][1]))
-       print("The y_intercept in this iteration is {}".format(b_curr[0]))
+       print ("The gradient of term 1 in this iteration is {}".format(m['m_curr_0']))
+       print ("The gradient of term 2 in this iteration is {}".format(m['m_curr_1']))
+       print("The y_intercept in this iteration is {}".format(b_curr))
        print("This is iteration number {} and it costs {}".format(i, cost))
        print('\n')
 
-       '''if abs(learning_rate*m_grad)<0.00001 and abs(learning_rate*b_grad)<0.00001:
+       if abs(learning_rate*m_grad)<0.00001 and abs(learning_rate*b_grad)<0.00001:
            print (learning_rate*m_grad)
            break
        
        if cost == 0:
            print("You have perfectly fit the data")
-           break'''
+           break
 
    
        #need to account for multiple independent variables 
